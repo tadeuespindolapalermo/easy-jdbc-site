91cbd9b37c62fe1e49118731755243a839d68606
Tadeu Espíndola Palermo <tepalermo@stefanini.com>
2020-09-08 23:48:31 -0300

    Update layout Home.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FeaturesComponent } from './features/features.component';
+import { HomeComponent } from './features/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
